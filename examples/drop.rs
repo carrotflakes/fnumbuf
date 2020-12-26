18e80998c84db5794d4a15b950e7c09bae3bf4ef
carrotflakes <carrotflakes@gmail.com>
2020-12-27 04:49:15 +0900

    add examples/drop.rs

---
+++ examples/drop.rs
@@ -0,0 +1,42 @@
+use fnumbuf::FnumBuf;
+
+#[derive(Debug)]
+struct ShowDrop;
+
+impl Drop for ShowDrop {
+    fn drop(&mut self) {
+        println!("drop!");
+    }
+}
+
+#[derive(Debug, fnum_derive::Fnum)]
+enum MyEnum {
+    A(ShowDrop),
+    B
+}
+
+fn main() {
+    {
+        println!("1");
+        let mut buf = FnumBuf::new();
+        buf.push(MyEnum::A(ShowDrop));
+        buf.push(MyEnum::B);
+        buf.push(MyEnum::A(ShowDrop));
+    }
+    {
+        println!("2");
+        let mut buf = FnumBuf::new();
+        buf.push(MyEnum::A(ShowDrop));
+        buf.push(MyEnum::B);
+        buf.push(MyEnum::A(ShowDrop));
+        buf.iter().for_each(|_| ());
+    }
+    {
+        println!("3");
+        let mut buf = FnumBuf::new();
+        buf.push(MyEnum::A(ShowDrop));
+        buf.push(MyEnum::B);
+        buf.push(MyEnum::A(ShowDrop));
+        buf.into_iter().for_each(|_| ());
+    }
+}
